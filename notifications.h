124d0fcd76806fe2f3100c955e4fbee6f32a7fdf
k.i.smith <k.i.smith@0014238c-9725-0410-8bf0-153065148e10>
2007-01-02 22:45:39 +0000

    Added growl support. Completely useless, but rather fun :)

---
+++ notifications.h
@@ -0,0 +1,31 @@
+/*
+ * Notifications.h - Header for event notifications.
+ * Copyright (C) 2006  Kevin Smith.
+ *
+ * This file is part of the MacAceUpdater program.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * as published by the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Notifications : NSObject {
+
+}
+
+- (void) announceMessageString:(NSString*) message;
+
+@end
