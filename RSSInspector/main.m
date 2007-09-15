91e6ada506f18f6021caaf030709d9fa70dffd1e
k.i.smith <k.i.smith@0014238c-9725-0410-8bf0-153065148e10>
2007-09-15 21:13:22 +0000

    Now reads in from RSS rather than HTML. Caution: currently segfaults deep within Cocoa for some reason I don't entirely understand; hints welcomed.

---
+++ RSSInspector/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RSSInspector
+//
+//  Created by Brent Simmons on Tue Jun 04 2002.
+//  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char *argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
